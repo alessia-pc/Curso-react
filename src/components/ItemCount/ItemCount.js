05d48aafe75ca0897115408e718b5b257336ce55
alessia-pc <pugaalessia@gmail.com>
2022-04-25 15:29:00 -0300

    cambios generales

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -2,49 +2,27 @@ import React from "react";
 import "./ItemCount.css";
 import { useState } from "react";
 
+function ItemCount({ stock, onAdd }) {
+  const [count, setCount] = useState(0);
 
-
-const ItemCount = (props) => {
-  const [count, setCount] = useState(1);
-
-  const onAdd = (condition) => {
-    if (condition === "+") {
+  function incrementar() {
+    if (count < stock) {
       setCount(count + 1);
     }
-    if (condition === "-") {
+  }
+
+  function decrementar() {
+    if (count > 0) {
       setCount(count - 1);
     }
-  };
-
-  const stock = 15;
-  const initial = 1;
-
+  }
   return (
-    <div className="BotonesProductos">
-      <div className="BotonesItemCount">
-        <button
-          onClick={() => {
-            if (count > initial) {
-              onAdd("-");
-            }
-          }}
-        >
-          -
-        </button>
-        {count}
-        <button
-          onClick={() => {
-            if (count < stock) {
-              onAdd("+");
-            }
-          }}
-        >
-          +
-        </button>
-        <button>Agregar al carrito</button>
-      </div>
+    <div>
+      <button onClick={decrementar}>-</button>
+      <span>{count}</span>
+      <button onClick={incrementar}>+</button>
+      <button onClick={() => onAdd(count)}> Agregar al carrito</button>
     </div>
   );
-};
-
+}
 export default ItemCount;
