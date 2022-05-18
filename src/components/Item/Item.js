304b51aa063b3b574cf8328d873e0c8ee62c49ca
alessia-pc <pugaalessia@gmail.com>
2022-05-18 03:44:26 -0300

    dividí la card por secciones

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -5,12 +5,18 @@ import { muestraEnMiles } from "../../services/muestraEnMiles";
 const Item = ({ nombre, img, precio, id, categoria }) => {
   return (
         <div className={categoria}>
-          <h3>{nombre}</h3>
-          <p>Precio: $ {muestraEnMiles(precio)}</p>
-          <img className="img-productos" src={img} alt={nombre} />
-          <Link to={`/detalle/${id}`}>
-            <button> Ver detalles </button>
-          </Link>
+          <div className="contenedor-card">
+            <div className="nombre-y-precio-item">
+              <h3>{nombre}</h3>
+              <p>Precio: $ {muestraEnMiles(precio)}</p>
+            </div>
+            <img className="img-productos" src={img} alt={nombre} />
+            <div className="link-ver-detalles">
+              <Link to={`/detalle/${id}`}>
+                <button> Ver detalles </button>
+              </Link>
+            </div>
+          </div>
         </div>
   );
 };
